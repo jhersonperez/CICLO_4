d67adecdb25a5eec19b6baa4bdaa0861de3d9836
Jherson Perez <jhersonperez3@gmail.com>
2022-11-30 14:31:58 -0500

    Despliegue 1 ciclo 4 - para subirlo a heroku

--- CONFIG/db.py
+++ CONFIG/db.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
 
-CONNECTION_STRING_CLOUD = "mongodb+srv://yeko:elyeko23@cluster0.k93xvrz.mongodb.net/test"
+CONNECTION_STRING_CLOUD = "mongodb+srv://yeko:elyeko12@cluster0.k93xvrz.mongodb.net/test"
 
 conn = MongoClient(CONNECTION_STRING_CLOUD)
